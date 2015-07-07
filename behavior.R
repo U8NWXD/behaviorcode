12b1cddfef6c39bbe7a5634acbd1ef2184d5495e
Katrina Kent <kkent17@stanford.edu>
2015-07-07 16:09:32 -0700

    jk messed up the commit it's here

--- behavior.R
+++ behavior.R
@@ -1252,11 +1252,12 @@ source("~/Desktop/Katrina/behavior_code/bootstrap_tests_June2013_STABLE.R")
 		write.csv(transProbsByGroup[[group]], file = paste(outfilePrefix, group, "transitionalprobabilities_datadump.csv", sep = "_"));
 		groupPMsAndCounts = list(probMats = groupPMs, counts = lapply(groupwiseLogs$groupData[[group]], function(d) {table(d$behavior)}));
 		probMat = .combineProbabilityMatrices(groupPMsAndCounts, groupwiseLogs$behnames, byTotal)$probMat;
-			# print(probMat);
-			# print(sum(probMat));
-			# print(apply(probMat, 1, sum));
-			# warning("Something is wrong about this probability matrix - the probabilities don't add up to 1. See Katrina for help.");
-		# } TODO TODO FIX THISSSSS
+		if ((byTotal && sum(probMat) != 1) || (!byTotal && max(abs(apply(probMat,1,sum) - 1)) > 1e-15)) {
+			print(probMat);
+			print(sum(probMat));
+			print(apply(probMat, 1, sum));
+			stop("Something is wrong about this probability matrix - the probabilities don't add up to 1. See Katrina for help.");
+		}
 		write.csv(probMat, file = paste(outfilePrefix, group, "transitionalprobabilities_average.csv", sep = "_"));
 	}
 	return(.runStats(dataByGroup = transProbsByGroup, outfilePrefix = paste(outfilePrefix, "transitionalprobabilities", sep = "_"),
