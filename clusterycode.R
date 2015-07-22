d78e63b4597f7dbc213fc7bc99657a3827da8653
Katrina Kent <kkent17@stanford.edu>
2015-07-22 11:14:11 -0700

    plotting for rosa

--- clusterycode.R
+++ clusterycode.R
@@ -40,3 +40,37 @@ library(WGCNA)
 	}
 	return(output);
 }
+
+.makeBoxplot = function(data, lognames, ...) {
+	verboseBoxplot(data, gsub("/.*$", '', gsub("darkgrey", "zzz", lognames)), border = c('darkblue', 'gold', 'grey', 'transparent'), names = c('dark blue', 'gold', 'unassigned', 'not included'), xlab = 'Module', notch = F, frame.plot = F, cex.axis = 1.1, ...)
+	pointsStaggered(1, data[gsub("/.*$", '', lognames) == "darkblue"], "darkblue")
+	pointsStaggered(2, data[gsub("/.*$", '', lognames) == "gold"], "gold")
+	pointsStaggered(3, data[gsub("/.*$", '', lognames) == "grey"], "grey")
+	pointsStaggered(4, data[gsub("/.*$", '', lognames) == "darkgrey"], "grey35")
+	# points(rep(1,length(data[gsub("/.*$", '', lognames) == "darkblue"])), data[gsub("/.*$", '', lognames) == "darkblue"], pch = 16, cex = 2, col = "darkblue")
+	# points(rep(2,length(data[gsub("/.*$", '', lognames) == "gold"])), data[gsub("/.*$", '', lognames) == "gold"], pch = 16, cex = 2, col = "gold")
+	# points(rep(3,length(data[gsub("/.*$", '', lognames) == "grey"])), data[gsub("/.*$", '', lognames) == "grey"], pch = 16, cex = 2, col = "grey")
+	# points(rep(4,length(data[gsub("/.*$", '', lognames) == "darkgrey"])), data[gsub("/.*$", '', lognames) == "darkgrey"], pch = 16, cex = 2, col = "grey35")
+}
+
+pointsStaggered = function(x, y, color) {
+	freqtable = table(y);
+	n = 1;
+	npointsPlotted = 0
+	while (sum(freqtable >= n)) {
+		toPlot = as.numeric(names(freqtable)[freqtable == n])
+		exes = x + (((1:n) - (n+1)/2) * .1);
+		for (i in exes) {
+			points(rep(i, length(toPlot)), toPlot, pch = 16, cex = 2, col = color);
+			npointsPlotted = npointsPlotted + length(toPlot)
+		}
+		n = n+1;
+	}
+	print(npointsPlotted)
+} 
+
+
+.makeBoxplot(x$durations[1,], names(counts), ylab = "Time attending to females (s)")
+.makeBoxplot(x$durations[2,], names(counts), ylab = "Time attending to males (s)")
+.makeBoxplot(x$durations[4,], names(counts), ylab = "Time maintaining territory (s)")
+.makeBoxplot(counts, names(counts), ylab = "Total number of behaviors")
